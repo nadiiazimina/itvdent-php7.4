d4419553971dc2c482ad845d6dfb8a146c6109bf
nadiiazimina <84657846+nadiiazimina@users.noreply.github.com>
2023-01-27 10:13:56 +0100

    13Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pickurovanadiia/itvdent-php7.4',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'efd3fefc583dcfb62fe315a42ffca6b012be3548',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fzaninotto/faker' => array(
+            'pretty_version' => 'v1.5.0',
+            'version' => '1.5.0.0',
+            'reference' => 'd0190b156bcca848d401fb80f31f504f37141c8d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fzaninotto/faker',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pickurovanadiia/itvdent-php7.4' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'efd3fefc583dcfb62fe315a42ffca6b012be3548',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
